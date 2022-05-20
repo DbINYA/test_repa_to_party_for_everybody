b2ad8832eb60a349a0733b6bbb41ef1e89bf57bc
BluesOfTheRedPlanet <94981361+BluesOfTheRedPlanet@users.noreply.github.com>
2022-05-20 20:16:15 +0300

    !!! app.jinja_env.globals.update(decode_img=decode_img) !!! ОТВЕЧАЕТ ЗА ДЕШИФРОВКУ АВАТАРОК, НЕ ТРОГАТЬ !!!

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ from flask import Flask
 from flask_login import LoginManager
 from data.topics import Topics
 
-
 seck = open('SECRET_KEY.txt', 'r').read()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = seck
 app.config['DEBUG'] = True
-
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -21,13 +19,16 @@ def load_user(user_id):
     return db_sess.query(User).get(user_id)
 
 
+app.jinja_env.globals.update(decode_img=decode_img)
+
 app.add_url_rule('/homeforum/<string:lang>/', view_func=home, methods=['GET', 'POST'])
 app.add_url_rule('/homeforum/<string:lang>/<int:page>', view_func=home, methods=['GET', 'POST'])
 app.add_url_rule('/homeforum/<string:lang>/signup', view_func=register, methods=['GET', 'POST'])
 app.add_url_rule('/homeforum/<string:lang>/signin', view_func=login, methods=['GET', 'POST'])
 app.add_url_rule('/homeforum/signout/', view_func=logout)
 app.add_url_rule('/homeforum/<string:lang>/addquestion', view_func=addquestion, methods=['GET', 'POST'])
-app.add_url_rule('/homeforum/<string:lang>/<int:id_topic>/<string:name_topic>', view_func=sometopic, methods=['GET', 'POST'])
+app.add_url_rule('/homeforum/<string:lang>/<int:id_topic>/<string:name_topic>', view_func=sometopic,
+                 methods=['GET', 'POST'])
 app.add_url_rule('/homeforum/<string:lang>/profile/<string:username_id>', view_func=profil)
 app.add_url_rule('/homeforum/<string:lang>/edit/<string:username_id>', view_func=profile_edit, methods=['GET', 'POST'])
 
